676bddff3550d56de85871eb42dddb56dee36435
Yarden Porat <yardenporat@gmail.com>
2022-01-19 11:35:38 +0200

    remove stuff

--- src/_wcs/simulations/new-comp/new-comp.demo.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { createDemo } from '@wixc3/react-simulation';
-import { NewComp } from '../../../new-comp/new-comp';
-
-export default createDemo({
-    name: 'NewComp',
-    demo: () => <NewComp />
-});
--- src/_wcs/simulations/new-componentz/new-componentz.demo.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { createDemo } from '@wixc3/react-simulation';
-import { NewComponentz } from '../../../new-componentz/new-componentz';
-
-export default createDemo({
-    name: 'NewComponentz',
-    demo: () => <NewComponentz />
-});
